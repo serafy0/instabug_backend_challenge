06d7e78bf8015902e5dd8dd31408e957ebae7d29
mohamedelserafy <elserafy02@gmail.com>
2022-05-29 15:51:01 +0200

    seed apps

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+apps = []
+(1..20).each do |i|
+  apps.push({ name: "app#{i}" })
+end
+
+App.create(apps)

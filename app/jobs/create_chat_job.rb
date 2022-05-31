bc12e91a0d953d795c147c75c22a9552056ba69f
mohamedelserafy <elserafy02@gmail.com>
2022-05-31 13:21:24 +0200

    fix chat's job

--- app/jobs/create_chat_job.rb
+++ app/jobs/create_chat_job.rb
@@ -4,7 +4,7 @@ class CreateChatJob < ApplicationJob
   def perform(token)
     Chat.transaction do
       chat_count = 0
-      last_chat_inserted = Chat.where(app_token: token).select(:number).order('number DESC').first
+      last_chat_inserted = Chat.where(app_token: token).select(:number).order(number: :desc).first
       chat_count = last_chat_inserted.number unless last_chat_inserted.nil?
 
       chat = Chat.new({ app_token: token, number: chat_count + 1 })

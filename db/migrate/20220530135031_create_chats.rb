dc7c277ec9d472b92d125c5fe737f0ff4b920639
mohamedelserafy <elserafy02@gmail.com>
2022-06-04 04:17:03 +0200

    index chats

--- db/migrate/20220530135031_create_chats.rb
+++ db/migrate/20220530135031_create_chats.rb
@@ -3,8 +3,9 @@ class CreateChats < ActiveRecord::Migration[7.0]
     create_table :chats do |t|
       t.integer :number
       t.string :app_token
-      
+
       t.timestamps
     end
+    add_index :chats, :app_token
   end
 end

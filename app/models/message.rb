38030748667e555884a702ba81443f048e550ecb
mohamedelserafy <elserafy02@gmail.com>
2022-06-04 11:56:49 +0200

    setup search

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,7 @@ class Message < ApplicationRecord
   validates :app_token, presence: true
   validates :chat_number, presence: true
   validates :text, presence: true
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :apps do
     resources :chats do
-      resources :messages
+      resources :messages do
+        post 'search', on: :collection
+      end
     end
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
